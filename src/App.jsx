c01f21ce0e15852fafaf599e86861b58ce104f85
GolbanEF <Golban.ef@yandex.ru>
2024-04-20 22:12:18 +0300

    go

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import YandexMain from './components/YandexMain';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <YandexMain/>
+    </div>
+  );
+}
+
+export default App;
